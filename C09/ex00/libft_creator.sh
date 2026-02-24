649d7161010ec7bcc37e30d05a57163e25a9686b
root <root@kiiroPC.localdomain>
2026-02-24 17:03:11 +0900

    2/24-42

---
+++ C09/ex00/libft_creator.sh
@@ -0,0 +1,8 @@
+cc -Wall -Wextra -Werror -c ft_putchar.c
+cc -Wall -Wextra -Werror -c ft_swap.c
+cc -Wall -Wextra -Werror -c ft_putstr.c
+cc -Wall -Wextra -Werror -c ft_strlen.c
+cc -Wall -Wextra -Werror -c ft_strcmp.c
+
+ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+ranlib libft.a
